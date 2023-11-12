a1fcdb60ea593f01df5aba2e03e35f27788b7ee7
RosalesJhordi <1428060@senati.pe>
2023-11-11 19:56:40 -0500

    Creacion de proyecto

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AppAsistencia</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-4xl font-bold">
+      inicio de appasistencia  
+    </h1>
+</body>
+</html>
